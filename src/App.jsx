381459fe0b86710d2946cbae280f831ddc357df6
EniolaCodes <adebimpeniola@gmail.com>
2025-05-26 14:28:21 +0100

    feat: review useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,29 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [car, setCar] = useState({
+    brand: "Benz",
+    model: "Lamborghini",
+    year: "2024",
+    color: "grey",
+  });
+  const updateColor = () => {
+    setCar((previousState) => {
+      return { ...previousState, color: "red" };
+    });
+  };
   return (
-    <div className="p-12">
-      <h2>Saheed AbdulRazaq</h2>
-      <Panel
-        title="About"
-        isActive={activeIndex === 0}
-        onShow={() => setActiveIndex(0)}
+    <div className="text-2xl font-extrabold bg-[#009E65] text-[#fff]">
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}
+      </p>
+      <button
+        onClick={updateColor}
+        className="cursor-pointer px-6 py-4 bg-amber-700 text-black"
       >
-        With a population of about 2 million
-      </Panel>
-      <Panel
-        title="Physics"
-        isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(1)}
-      >
-        What is isotopes
-      </Panel>
-      <Panel
-        title="bio"
-        isActive={activeIndex === 2}
-        onShow={() => setActiveIndex(2)}
-      >
-        What is isotopes
-      </Panel>
-      <Panel
-        title="chemistry"
-        isActive={activeIndex === 3}
-        onShow={() => setActiveIndex(3)}
-      >
-        gbasgbos
-      </Panel>
+        Change color
+      </button>
     </div>
-  );
-}
-
-function Panel({ title, children, isActive, onShow }) {
-  return (
-    <section className="border-2 border-red-600 p-4">
-      <h3 className="text-xl font-bold">{title}</h3>
-      {isActive ? (
-        <p className="text-[14px] font-semibold">{children}</p>
-      ) : (
-        <button
-          className="border px-4 py-6 hover:text-amber-400"
-          onClick={onShow}
-        >
-          Show
-        </button>
-      )}
-    </section>
   );
 }
