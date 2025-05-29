b5614d852a6713445f6807f2f516a823d3c56161
EniolaCodes <adebimpeniola@gmail.com>
2025-05-29 14:43:47 +0100

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
-
+import React, { useReducer } from "react";
 export default function App() {
-  const [car, setCar] = useState({
-    brand: "Benz",
-    model: "Lamborghini",
-    year: "2024",
-    color: "grey",
-  });
-  const updateColor = () => {
-    setCar((previousState) => {
-      return { ...previousState, color: "red" };
-    });
-  };
+  const [state, dispatch] = useReducer(counterReducer, initialCountState);
   return (
-    <div className="text-2xl font-extrabold bg-[#009E65] text-[#fff]">
-      <h1>My {car.brand}</h1>
-      <p>
-        It is a {car.color} {car.model} from {car.year}
-      </p>
-      <button
-        onClick={updateColor}
-        className="cursor-pointer px-6 py-4 bg-amber-700 text-black"
-      >
-        Change color
-      </button>
+    <div className=" bg-green-100 p-20 ">
+      <h1>Count: {state.count}</h1>
+      <div className="flex flex-col space-y-5">
+        <button
+          className="bg-amber-900 px-6 py-3 w-[200px]"
+          onClick={() => dispatch({ type: "increment" })}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-amber-900 px-6 py-3 w-[200px]"
+          onClick={() => dispatch({ type: "decrement" })}
+        >
+          Drecrement
+        </button>
+        <button
+          className="bg-amber-900 px-6 py-3 w-[200px]"
+          onClick={() => dispatch({ type: "reset" })}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      throw new Error();
+  }
+}
+const initialCountState = { count: 0 };
+
+//memoization
+//useCallback function caching
+//useMemo value
